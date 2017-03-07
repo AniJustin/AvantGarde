3a02977633c745271a506ef088ee729ad305ec5e
Sharad technology <sharadtechnology@Sharads-MacBook-Pro.local>
2017-03-07 09:38:36 +0400

    Initial Commit

---
+++ AvantGarde/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AvantGarde
+//
+//  Created by Sharad technology on 3/7/17.
+//  Copyright © 2017 Sharad technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
